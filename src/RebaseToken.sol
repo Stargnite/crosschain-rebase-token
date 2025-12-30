26343eaab12e69aef998f1a3f3eb9b165d567c53
shobam <tobyolamide22@gmail.com>
2025-12-30 18:24:53 +0100

    completed rebase token & test

---
+++ src/RebaseToken.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+
+/**
+ * @author Oluwatobi Sobayo
+ * @title RebaseToken
+ * @dev A Cross-chain rebase token that incentivizes users when they deposit after a long period of time.
+ *
+ */
+
+contract RebaseToken is ERC20, Ownable, AccessControl {
+    error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
+
+    uint256 private constant PRECISION_FACTOR = 1e18;
+    bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
+    uint256 private s_interestRate = (5 * PRECISION_FACTOR) / 1e8; // 0.00000005% per second
+    mapping(address => uint256) private s_userInterestRate;
+    mapping(address => uint256) private s_userLastUpdatedTimestamp;
+
+    event InterestRateSet(uint256 newInterestRate);
+
+    constructor() ERC20("RebaseToken", "RBT") Ownable(msg.sender) {}
+
+    function grantMintAndBurnRole(address _account) external onlyOwner {
+        _grantRole(MINT_AND_BURN_ROLE, _account);
+    }
+
+    function setInterestRate(uint256 _newInterestRate) external onlyOwner {
+        if (_newInterestRate >= s_interestRate) {
+            revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
+        }
+        s_interestRate = _newInterestRate;
+        emit InterestRateSet(_newInterestRate);
+    }
+
+    function principleBalanceOf(address _user) external view returns (uint256) {
+        return super.balanceOf(_user);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
+        _mintAccruedInterest(_to);
+        s_userInterestRate[_to] = s_interestRate;
+        _mint(_to, _amount);
+    }
+
+    function burn(address _from, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
+        _mintAccruedInterest(_from);
+        _burn(_from, _amount);
+    }
+
+    function balanceOf(address _user) public view override returns (uint256) {
+        // get user's minted token balance
+        // calculate the interest user has gained over time by multiplying the principle balance with the interest rate accumulated since the user deposited
+        return super.balanceOf(_user) * _calculateUserAccumulatedInterestSinceLastUpdate(_user) / PRECISION_FACTOR;
+    }
+
+    function transfer(address _recipient, uint256 _amount) public override returns (bool) {
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(msg.sender);
+        }
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
+        }
+
+        return super.transfer(_recipient, _amount);
+    }
+
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_sender);
+        }
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[_sender];
+        }
+
+        return super.transferFrom(_sender, _recipient, _amount);
+    }
+
+    /**
+     * @notice This function calculates the interest accumulated for a user since their last update
+     * @param _user The address of the user which the interest is being calculated for
+     * @return linearInterest The interest accumulated for the user since their last update
+     */
+    function _calculateUserAccumulatedInterestSinceLastUpdate(address _user)
+        internal
+        view
+        returns (uint256 linearInterest)
+    {
+        // Interest has a linear growth over time
+        uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
+        linearInterest = PRECISION_FACTOR + (s_userInterestRate[_user] * timeElapsed);
+    }
+
+    function _mintAccruedInterest(address _user) internal {
+        // Get the user's deposited and minted principle balance
+        uint256 previousPrincipleBalance = super.balanceOf(_user);
+        // calculate the interest user has gained over time with the current balance
+        uint256 currentBalance = balanceOf(_user);
+        // calculate the number of tokens as the interest the user gained.
+        uint256 balanceIncrease = currentBalance - previousPrincipleBalance;
+        // Set user's last data timestamp
+        s_userLastUpdatedTimestamp[_user] = block.timestamp;
+        _mint(_user, balanceIncrease);
+    }
+
+    function getInterestRate() external view returns (uint256) {
+        return s_interestRate;
+    }
+
+    function getUserInterestRate(address user) public view returns (uint256) {
+        return s_userInterestRate[user];
+    }
+
+    function getPrecisionFactor() external pure returns (uint256) {
+        return PRECISION_FACTOR;
+    }
+}
