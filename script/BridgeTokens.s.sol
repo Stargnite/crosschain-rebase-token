86a95a81c034cda2b249802e8d9532fdc5ca0411
shobam <tobyolamide22@gmail.com>
2026-01-08 18:46:26 +0100

    added deploy scripts

--- script/BridgeTokens.s.sol
+++ script/BridgeTokens.s.sol
@@ -17,10 +17,10 @@ contract BridgeTokens is Script {
         Client.EVMTokenAmount memory tokenAmounts = new Client.EVMTokenAmount[](1);
         tokenAmounts[0] = Client.EVMTokenAmount({
             token: tokenToSendAddress,
-            amount: amountToSend,
+            amount: amountToSend
         });
         vm.startBroadcast();
-        Client.EVM2AnyMessage message = Client.EVM2AnyMessage({
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
             receiver: abi.encode(receiverAddress),
             data: "",
             tokenAmounts: tokenAmounts,
